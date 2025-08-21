5129c13881262e901e8ff222d156fd1a9d5d167c
itspramod21 <35529335+itspramod21@users.noreply.github.com>
2025-08-21 21:20:44 +0800

    Update azure.Python.app.py

--- azure/azure.Python.app/azure.Python.app/azure.Python.app.py
+++ azure/azure.Python.app/azure.Python.app/azure.Python.app.py
@@ -4,15 +4,12 @@ if __name__ == "__main__":
     from azure.mgmt.compute import ComputeManagementClient
     from azure.mgmt.network import NetworkManagementClient
     from azure.mgmt.compute.models import DiskCreateOption
-SUBSCRIPTION_ID = '9cd085a2-228d-4568-b496-bf4da6ed72d8'
+
 GROUP_NAME = 'RG-w2016con1'
 LOCATION = 'southeastasia'
 VM_NAME = 'w2016con1'
 def get_credentials():
-    credentials = ServicePrincipalCredentials(
-        client_id = '126ee1bb-512c-42a2-a00c-84e35c5d4963',
-        secret = '+NuwBXSoZ5F+UdfxLYa+6d4WQJsV//Sbd/xo75WyfF0=',
-        tenant = '823e1f7a-5d1c-4595-8db2-e303ce3ffb4f'
+    
 ) 
     return credentials
 credentials = get_credentials()
