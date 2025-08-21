588582f89643efe2ef76324cc5a879769833eafc
itspramod21 <35529335+itspramod21@users.noreply.github.com>
2025-08-21 21:21:44 +0800

    Update zermftracker.py

--- mf/zermftracker.py
+++ mf/zermftracker.py
@@ -1,15 +1,14 @@
 from kiteconnect import KiteConnect
 
 # Replace with your actual API key and API secret from Zerodha
-api_key = "ndbndz06ti49xlpk"
-api_secret = "zggr5d7di1volyxiwcpmusz8fbm0ixs8"
+
 
 kite = KiteConnect(api_key=api_key)
 
 print(kite.login_url())
 
 # You need to generate a request token via Zerodha login flow and set it here
-request_token = "BjBXI7x004RXMMW8PS0K1D2POvubOQPT"
+
 
 # Generate access token
 data = kite.generate_session(request_token, api_secret=api_secret)
